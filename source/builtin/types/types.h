a8a30eb3cff5100935bf987aecea0014dc7f4560
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-01-12 16:09:58 -0300

    Update types.h

--- source/builtin/types/types.h
+++ source/builtin/types/types.h
@@ -6,7 +6,7 @@ const unsigned short true = 1;
 const unsigned short false = 0;
 
 // using size 'long' is big enough and allows easy math with indexes;
-// also BaseOne string functions protect it to become negative
+// also BaseOne string functions protect it from become negative
 typedef struct {
     long size;
     char *data;    
