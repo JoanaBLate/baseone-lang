633487c44d853e0a31153a4844b4e23ac799932b
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-01-13 10:48:34 -0300

    Add files via upload

--- source/builtin/memory/memory.h
+++ source/builtin/memory/memory.h
@@ -1,1 +1,11 @@
 
+void *memoryAllocate(size_t size)
+{
+    size_t *address = malloc(size);
+    
+    if (address != NULL) { return address; }
+
+    printf("\nERROR: failed to allocate %lu bytes in heap\n", size);
+    
+    exit(1);
+}
