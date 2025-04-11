73ded0dbfe2139e7c23e14b1bb94069b69c15efa
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-04-11 11:57:34 -0300

    Update file--test.h

--- source/builtin--test/file/file--test.h
+++ source/builtin--test/file/file--test.h
@@ -1,1 +1,17 @@
+// # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
 
+void testReadTextFile()
+{
+    printf("- testing readTextFile\n"); 
+    
+    String filename = createStringFromLiteral("../builtin/builtin.h");        
+    String file = readTextFile(&filename);
+
+    String string = createStringSub(&file, 1, 18);
+    String expected = createStringFromLiteral("// # Copyright (c)");
+
+    if (! stringsAreEqual(&expected, &string)) { 
+        printf("readTextFile FAILS!\n"); 
+        exit(1);
+    }
+}
