5054078fe62c3b295e10cd48a9aa88295957c9af
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-04-11 11:31:19 -0300

    Update types.h

--- source/builtin/types/types.h
+++ source/builtin/types/types.h
@@ -1,14 +1,14 @@
 // # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
 
+typedef unsigned short byte;
+
 typedef unsigned short bool;
 
 const unsigned short true = 1;
 const unsigned short false = 0;
 
-// using size 'long' is big enough and allows easy math with indexes;
-// also BaseOne string functions protect it from become negative
-typedef struct {
-    long size;
-    char *data;    
-} String;
-
+// why using 'long' for sizes instead of 'unsigned long' (size_t)?
+// -- 'long' is big enough (half of the adressable memory)
+// -- unsigned integers are more complicated to handle (logical comparison with negative numbers)
+// -- BaseOne builtin functions assure that size/capacity/length will never get negative value
+// -- while this is about the inner C code, it follows the BaseOne principle: there is no 'unsigned'
