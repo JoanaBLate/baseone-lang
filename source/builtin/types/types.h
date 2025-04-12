9ca137887a53034971083cf42c8909b6f7bd96c0
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-04-12 17:05:32 -0300

    Update types.h

--- source/builtin/types/types.h
+++ source/builtin/types/types.h
@@ -1,5 +1,11 @@
 // # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
 
+// why using 'long' for sizes instead of 'unsigned long' (size_t)?
+// -- 'long' is big enough (half of the adressable memory)
+// -- unsigned integers are more complicated to handle (logical comparison with negative numbers)
+// -- BaseOne builtin functions assure that size/capacity/length will never get negative value
+// -- while this is about the inner C code, it follows the BaseOne principle: there is no 'unsigned'
+
 typedef unsigned short byte;
 
 typedef unsigned short bool;
@@ -7,8 +13,22 @@ typedef unsigned short bool;
 const unsigned short true = 1;
 const unsigned short false = 0;
 
-// why using 'long' for sizes instead of 'unsigned long' (size_t)?
-// -- 'long' is big enough (half of the adressable memory)
-// -- unsigned integers are more complicated to handle (logical comparison with negative numbers)
-// -- BaseOne builtin functions assure that size/capacity/length will never get negative value
-// -- while this is about the inner C code, it follows the BaseOne principle: there is no 'unsigned'
+typedef struct
+{
+    long value;
+    bool isNull;
+} NullLong;
+
+NullLong createNullLong()
+{
+    NullLong number = { 0, true };
+    
+    return number;
+}
+
+NullLong createNullableLong(long value, bool isNull)
+{
+    NullLong number = { value, isNull };
+    
+    return number;
+}
