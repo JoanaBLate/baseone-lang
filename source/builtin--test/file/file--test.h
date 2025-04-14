483e96bf28e8f984b4d5eccffc7509514280d592
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-04-14 20:53:57 -0300

    Add files via upload

--- source/builtin--test/file/file--test.h
+++ source/builtin--test/file/file--test.h
@@ -1,16 +1,17 @@
 // # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
 
+
 void testReadTextFile()
 {
     printf("- testing readTextFile\n"); 
     
-    String filename = makeStringFromLiteral("../builtin/builtin.h");        
-    String file = readTextFile(&filename);
+    String* filename = makeStringFromLiteral("../builtin/builtin.h");        
+    String* file = readTextFile(filename);
 
-    String string = createStringSub(&file, 1, 18);
-    String expected = makeStringFromLiteral("// # Copyright (c)");
+    String* string = stringSlice(file, 1, 18);
+    String* expected = makeStringFromLiteral("// # Copyright (c)");
 
-    if (! stringsAreEqual(&expected, &string)) { 
+    if (! stringsAreEqual(expected, string)) { 
         printf("readTextFile FAILS!\n"); 
         exit(1);
     }
