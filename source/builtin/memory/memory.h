70035ef9f4050339d9a3aab210edf2b360f42947
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-04-22 07:41:12 -0300

    Add files via upload

--- source/builtin/memory/memory.h
+++ source/builtin/memory/memory.h
@@ -1,9 +1,12 @@
 // # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
 
+
 // pointer must be unsigned long, because must be able to access all RAM bytes
 
-void* allocateMemory(unsigned long size)
+void* heapAllocate(long size)
 {
+    if (size < 1) { return NULL; }
+    
     unsigned long* address = malloc(size);
     
     if (address != NULL) { return address; }
@@ -13,14 +16,21 @@ void* allocateMemory(unsigned long size)
     exit(1);
 }
 
-void* allocateMemoryClean(unsigned long amount, unsigned long size)
+void* heapAllocateClean(long size)
 {
-    unsigned long* address = calloc(amount, size);
+    if (size < 1) { return NULL; }
+    
+    unsigned long* address = calloc(1, size);
     
     if (address != NULL) { return address; }
 
     printf("\nERROR: failed to allocate %lu bytes in heap\n", size);
     
     exit(1);
+}
+
+void heapRelease(void* pointer)
+{
+    free(pointer);
 }
 
